6f115e26a464fea622b5d111d69cfad43f36eb12
Steven Burrell <sburrell23@gmail.com>
2025-11-26 09:05:42 -0700

    DEPLOY

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { Game } from './game/Game'
 const canvas = document.getElementById('gameCanvas') as HTMLCanvasElement
 const game = new Game(canvas)
 
+// Export game to window for debugging/accessibility
+;(window as any).game = game
+
 // Don't start automatically - wait for start button click
 // game.start() will be called when the start button is clicked
 
